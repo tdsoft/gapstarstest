01472cf876e56b67a4603c66a4d95ab08b64d334
Tharindu <tharindu@jrcsoftware.com>
2021-03-11 09:15:44 +0530

    token updated

---
+++ GapstarsTest/app/src/main/java/com/tharindu/damintha/gapstarstest/common/Constants.kt
@@ -0,0 +1,3 @@
+package com.tharindu.damintha.gapstarstest.common
+
+const val GIT_KEY = "a06a0903a68b3ed21ae2019dfa3e443ba20cfa0d"
--- GapstarsTest/app/src/main/java/com/tharindu/damintha/gapstarstest/utils/AuthorizationInterceptor.kt
+++ GapstarsTest/app/src/main/java/com/tharindu/damintha/gapstarstest/utils/AuthorizationInterceptor.kt
@@ -1,12 +1,14 @@
 package com.tharindu.damintha.gapstarstest.utils
 
+import com.tharindu.damintha.gapstarstest.common.GIT_KEY
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthorizationInterceptor() : Interceptor {
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer a06a0903a68b3ed21ae2019dfa3e443ba20cfa0d")
+            .addHeader("Authorization", "Bearer $GIT_KEY")
             .build()
 
         return chain.proceed(request)
